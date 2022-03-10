421932079f392b03b2bee2cf4554878ec31caef8
kawsar4148 <kawsarahmed4148@gmail.com>
2022-03-10 10:26:32 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,8 @@
+const anthem = 'Amar Sonar Bangla Ami Tumay Valobashi'
+
+// Convert toLowerCase
+const lowerCase = anthem.toLowerCase()
+// Convert toUpperCase
+const myName = 'Kawsar Ahmed'
+const upperCase = myName.toUpperCase()
+console.log(upperCase);
